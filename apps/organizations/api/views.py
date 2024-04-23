ff6d0a6ad21b3ef3f2d088df2f29c90e3ff6e149
Lucas <lucasoliveirars22@gmail.com>
2024-04-23 14:07:46 -0300

    add validação no endpoint create de Organization

--- apps/organizations/api/views.py
+++ apps/organizations/api/views.py
@@ -3,17 +3,22 @@ from rest_framework.response import Response
 from apps.organizations.api.serializers import OrganizationSerializer
 from apps.events.models import Organization
 
+
 class OrganizationViewSet(viewsets.ModelViewSet):
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
 
     def create(self, request, *args, **kwargs):
-        # Adicione lógica para associar o evento à organização atualmente logada
+        # Add logic to associate the event with the currently logged in organization
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            # Associe o evento à organização atualmente logada
-            serializer.validated_data['organization'] = request.user.organization  
+            # Associate the event with the currently logged in organization
+            if request.user.organization:
+                serializer.validated_data["organization"] = request.user.organization
+
             self.perform_create(serializer)
             headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED, headers=headers
+            )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
