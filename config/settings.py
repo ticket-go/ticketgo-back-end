41597ac66d531c3e28caadba2dcf0f8e58f37fc3
Michael <michaelcesartools@hotmail.com>
2024-07-12 16:50:03 -0300

    Development (#66)
    Co-authored-by: Henrique Eduardo <azevedo.henrique@escolar.ifrn.edu.br>
    Co-authored-by: Henrique <heduardocr7@gmail.com>

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(BASE_DIR, "apps/templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
