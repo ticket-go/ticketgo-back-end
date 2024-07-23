c533cc5e504c47e37bceb979d2cf68376618bcd0
Michael <michaelcesartools@hotmail.com>
2024-07-23 15:05:21 -0300

    Development (#67)
    Co-authored-by: Henrique Eduardo <azevedo.henrique@escolar.ifrn.edu.br>
    Co-authored-by: Henrique <heduardocr7@gmail.com>

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
 from apps.users.api.views import (
-    LoginViewSet,
+    #LoginViewSet,
     CustomUserViewSet,
     CustomUserChangePasswordViewSet,
     LogoutViewSet,
     SocialLoginViewSet,
 )
 
+from drf_audit_trail.integrations.rest_framework_simplejwt import DRFAuditTrailTokenObtainPairView
+
 urlpatterns = [
     path("register/", CustomUserViewSet.as_view(), name="register"),
-    path("login/", LoginViewSet.as_view(), name="login"),
+    path("login/", DRFAuditTrailTokenObtainPairView.as_view(), name="login"),
     path("logout/", LogoutViewSet.as_view(), name="logout"),
     path("social-login/<str:provider>/", SocialLoginViewSet.as_view()),
     path(
