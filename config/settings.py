8e1756225803d709ff1212423cc093447ec81787
Lucas <lucasoliveirars22@gmail.com>
2024-07-31 10:53:09 -0300

    blackformatter settings

--- config/settings.py
+++ config/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # django-allauth
     "allauth.account.middleware.AccountMiddleware",
-    #drf-audit-trail
+    # drf-audit-trail
     "drf_audit_trail.middleware.RequestLoginAuditEventMiddleware",
 ]
 
@@ -181,7 +181,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "USER_ID_FIELD": "user_id",
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
@@ -193,15 +193,15 @@ SPECTACULAR_SETTINGS = {
 }
 
 
-# django.core.mail 
+# django.core.mail
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = (os.getenv("EMAIL_HOST"))
-EMAIL_PORT = (os.getenv("EMAIL_PORT"))
-EMAIL_USE_TLS = (os.getenv("EMAIL_USE_TLS"))
-EMAIL_HOST_USER = (os.getenv("EMAIL_HOST_USER"))
-EMAIL_HOST_PASSWORD = (os.getenv("EMAIL_HOST_PASSWORD"))
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 
-#drf-audit-trail
+# drf-audit-trail
 DRF_AUDIT_TRAIL_REQUEST_AUDIT_URLS = [r"^.*?/"]
 DRF_AUDIT_TRAIL_AUTH_URL = "/user/login/"
