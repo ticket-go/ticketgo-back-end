e4ae9d498385fcaddf2c6f3c39b23675c2252b1f
Lucas <lucasoliveirars22@gmail.com>
2024-08-15 15:49:36 -0300

    fix bug

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -8,10 +8,10 @@ from apps.users import models
 class CustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     address = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Address.objects.all(), required=False
+        queryset=Address.objects.all(), required=False
     )
     organization = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Organization.objects.all(), required=False
+        queryset=Organization.objects.all(), required=False
     )
 
     class Meta:
