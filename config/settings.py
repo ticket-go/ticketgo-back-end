b610699249f1a80218e36ae2730789b89cb709f7
Lucas <lucasoliveirars22@gmail.com>
2024-08-16 19:49:03 -0300

    add cors mobile + logout permission

--- config/settings.py
+++ config/settings.py
@@ -17,11 +17,14 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "ticketgo-backend-dev.onrender.com",
+    "10.0.2.2",
 ]
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://ticketgo-front-end-dev.onrender.com",
+    "https://ticketgo-frontend-dev.onrender.com",
+    "http://10.0.2.2:8000",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
