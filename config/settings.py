037284b1a8180945ec43a55293a13d7c9311018d
Lucas <lucasoliveirars22@gmail.com>
2024-09-03 23:20:30 -0300

    ajustes no settings

--- config/settings.py
+++ config/settings.py
@@ -147,13 +147,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Static
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Media
