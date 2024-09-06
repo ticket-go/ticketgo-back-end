c9de684e99074d193b0067c0b2f072a0e914f260
michaelcesar <michaelcesartools@hotmail.com>
2024-09-06 17:11:34 -0300

    test login user fail

--- apps/users/tests/test_views.py
+++ apps/users/tests/test_views.py
@@ -3,17 +3,21 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 from apps.users.models import CustomUser
 
+username = "michael"
+email = "michael@michael.com"
+password = "michael"
+cpf = "12345678909"
+
+
 @pytest.mark.django_db
 def test_create_user():
     client = APIClient()
-
-    username = "michael"
     
     user_data = {
         "username": username,
-        "email": "newuser@example.com",
-        "password": "password123",
-        "cpf": "12345678909"
+        "email": email,
+        "password": password,
+        "cpf": cpf
     }
  
     response = client.post(reverse('user-list'), user_data, format='json')
@@ -24,3 +28,26 @@ def test_create_user():
     assert "TicketGo" in response.data
     expected_message = f"O usuário {CustomUser.objects.get(username=username).user_id} - {username} foi registrado com sucesso."
     assert response.data["TicketGo"] == expected_message
+
+
+@pytest.mark.django_db
+def test_login_user_fail():
+    client = APIClient()
+
+    CustomUser.objects.create_user(
+        username=username, 
+        email=email, 
+        password=password, 
+        cpf=cpf
+    )
+
+    login_data = {
+        "username": username,
+        "password": "michael1"
+    }
+
+    response = client.post(reverse('login'), login_data, format='json')
+
+    assert response.status_code == 401
+    assert "access_token" not in response.data
+    assert "refresh_token" not in response.data
